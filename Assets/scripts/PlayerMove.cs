7f360f96e4828c3c58508aaee4eb1614093fc55c
nahjh2 <nahjh2@google.com>
2021-03-21 22:33:27 +0900

    raycast rigidbody2d spriterenderer animation etc

---
+++ Assets/scripts/PlayerMove.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float maxSpeed;
+    public float jumpPower;
+    public int jumpAble;
+    public int jumpCount;
+
+    Rigidbody2D rigid;
+    SpriteRenderer spriteRenderer;
+    Animator animator;
+
+    private void Awake() {
+        rigid = GetComponent<Rigidbody2D>();     
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update() {
+        // 점프
+        if (jumpCount < jumpAble && Input.GetButtonDown("Jump")) {
+            jumpCount++;
+            animator.SetBool("isJumping", true);
+            rigid.AddForce(
+                Vector2.up * jumpPower,
+                ForceMode2D.Impulse
+            );
+        }
+
+        // 멈출때 속도
+        if (Input.GetButtonUp("Horizontal")) {
+            rigid.velocity = new Vector2(0.5f * rigid.velocity.normalized.x, rigid.velocity.y);
+        }
+
+        // 캐릭터 이미지 방향 전환
+        if(Input.GetButtonDown("Horizontal"))
+           spriteRenderer.flipX = (Input.GetAxisRaw("Horizontal") == -1);
+
+        // 애니메이션 전환 (달리기 <-> 대기)
+        if (Mathf.Abs(rigid.velocity.x) < 0.3f)
+            animator.SetBool("isRunning", false);
+        else
+            animator.SetBool("isRunning", true);
+    }
+
+    void FixedUpdate()
+    {
+        // 좌우 움직이기
+        rigid.AddForce(
+            Vector2.right * Input.GetAxisRaw("Horizontal"),
+            ForceMode2D.Impulse
+        );
+
+        // 속도 제한
+        if(rigid.velocity.x > maxSpeed)
+            rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
+        if(rigid.velocity.x < -maxSpeed)
+            rigid.velocity = new Vector2(-maxSpeed, rigid.velocity.y);
+
+        // 점프하고 내려올시 바닥에 닫는지 확인
+        if(jumpCount > 0) {
+            if(rigid.velocity.y < 0) {
+                Debug.DrawRay(rigid.position, Vector3.down, new Color(0,1,0));  // 에디터 상에서 Ray를 그려주는 디버깅용 함수
+                RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, Vector2.down, 1, LayerMask.GetMask("Platform"));
+                if(rayHit.collider != null) {
+                    if(rayHit.distance < 0.6f) {
+                        animator.SetBool("isJumping", false);
+                        jumpCount = 0;
+                    }
+                }
+            }
+        }
+    }
+}
--- readme.md
+++ readme.md
@@ -8,6 +8,9 @@
 - [Camera](#camera)
 - [Animation](#animation)
 - [Project Settings](#project-settings)
+- [Core Module](#core-module)
+- [Physics 2D Module](#physics-2d-module)
+- [Component](#component)
 
 ---
 
@@ -37,11 +40,24 @@ Projection :
 
 ---
 
-## Animation
+## [Animation](https://youtu.be/Z4iULRbiSTg?list=PLO-mt5Iu5TeYI4dbYwWP8JqZMC9iuUIW2&t=1258)
 
 ```
 Animator :
     애니메이션을 관리하는 컴포넌트
+    애니메이터 매개변수를 기준으로 상태를 바꿀 수 있다.
+    Has Exit Time 옵션을 체크시 지금 에니메이션이 끝나기 전까지 안넘어감.
+
+    Transition :
+        애니메이션 상태를 옮겨가는 통로
+        애니메이션 창에서 우클릭 해서 만들 수 있다.
+
+
+    Animator.SetBool("parameter name", true) :
+        animator에 설정된 해당하는 이름의 논리값을 설정 true 대신 false 가능
+
+
+
 ```
 
 ---
@@ -52,5 +68,70 @@ Animator :
 Physics 2D :
     Default Contact Offset :
         충돌 여백
+
+```
+
+---
+
+## [Core Module](https://docs.unity3d.com/ScriptReference/UnityEngine.CoreModule.html)
+
+```
+Vector2 :
+    Vector2.Normalize() :
+        public void Normalize();
+        크기가 1인 백터로 변환
+
+    Vector2.Normalized :
+        public Vector2 normalized;
+        크기가 1인 백터를 반환(Read Only)
+
+SpriteRenderer :
+    Renders a Sprite for 2D graphics.
+
+    Flip :
+        스프라이트를 뒤집는 옵션
+
+LayerMask :
+    물리 효과를 구분하는 거
+
+    LayerMask.GetMask("layerName") :
+        해당하는 이름의 레이어의 숫자를 리턴함
+
+
+```
+
+---
+
+## [Physics 2D Module](https://docs.unity3d.com/ScriptReference/UnityEngine.PhysicsModule.html)
+
+```
+Global settings and helpers for 2D physics.
+
+Rigidbody2D :
+    Control of an object's position through physics simulation.
+
+    velocity :
+        public Vector2 velocity;
+
+    Freeze Rotation 옵션 :
+        체크시 오브젝트가 회전하지 않음
+
+RaycastHit2D :
+    Ray에 닿은 오브젝트
+
+Physics2D.Raycast( origin, direction, distance, layerMask ) :
+    origin, direction은 Vector2
+    distance는 float
+    layerMask는 int
+
+    origin에서 direction방향으로 distance만큼 ray를 쏴서 처음 만나는 layerMask에 있는 오브젝트의 정보를 반환
+
+```
+
+---
+
+## [Component](https://docs.unity3d.com/ScriptReference/Component.html)
+
+```
 
 ```
